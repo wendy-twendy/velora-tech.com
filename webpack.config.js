0c0a6ece1d84a60565255dd21669372c2cfc11f4
endi <unejamendi@gmail.com>
2025-03-12 23:51:37 +0100

    Fix deployment issue

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = (env, argv) => {
     },
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, './'),
       publicPath: '/'
     },
     plugins: [
